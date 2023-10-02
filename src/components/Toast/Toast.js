36522bc1d3393919318da5e7180fefb7b5fb8661
Diederik van Remoortere <diederik.van.remoortere@gmail.com>
2023-10-02 23:00:53 +0200

    add the icon

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -25,10 +25,12 @@ function Toast({
   variant = VARIANT_OPTIONS[0],
   onClose,
 }) {
+  const Icon = ICONS_BY_VARIANT[variant];
+
   return (
     <div className={`${styles.toast} ${styles[variant]}`}>
       <div className={styles.iconContainer}>
-        <Info size={24} />
+        <Icon size={24} />
       </div>
       <p className={styles.content}>{message}</p>
       <button className={styles.closeButton} onClick={onClose}>
