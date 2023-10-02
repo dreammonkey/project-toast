3afb40cb3afcaf85b1c2e8473bece042f7c4a9bf
Diederik van Remoortere <diederik.van.remoortere@gmail.com>
2023-10-03 00:09:31 +0200

    move provider to index

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,14 +2,11 @@ import React from "react";
 
 import ToastPlayground from "../ToastPlayground";
 import Footer from "../Footer";
-import ToastProvider from "../ToastProvider/ToastProvider";
 
 function App() {
   return (
     <>
-      <ToastProvider>
-        <ToastPlayground />
-      </ToastProvider>
+      <ToastPlayground />
       <Footer />
     </>
   );
